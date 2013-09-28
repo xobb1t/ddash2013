218a6d7ecb643411eba0aeb47bdc73fa6bde2fb6
Dima Kukushkin <dima@kukushkin.me>
2013-09-28 15:00:48 +0400

    Tune login view

--- apps/private/views.py
+++ apps/private/views.py
@@ -7,14 +7,11 @@ from .forms import LoginForm
 
 
 def login_view(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST, organization=request.organization)
-        if form.is_valid():
-            login(request, form.user_cache)
-            next = request.GET.get('next')
-            return redirect(next or settings.LOGIN_REDIRECT_URL)
-    else:
-        form = LoginForm(organization=request.organization)
+    form = LoginForm(request.POST or None, organization=request.organization)
+    if form.is_valid():
+        login(request, form.user_cache)
+        next = request.GET.get('next')
+        return redirect(next or settings.LOGIN_REDIRECT_URL)
     return render(request, 'private/login.html', {
         'form': form
     })
