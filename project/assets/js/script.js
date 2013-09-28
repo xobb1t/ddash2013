40c1c1884ceaca959a49129db13b51e43cd45829
Trolev <trolev@gmail.com>
2013-09-29 00:15:41 +0400

    add template organizetions.html

--- project/assets/js/script.js
+++ project/assets/js/script.js
@@ -1,5 +1,20 @@
 //= require_self
 
 $(document).ready(function(){
-  // alert(4);
+
+
+
+  (function(){
+    var $edit_name = $('#js-edit-name'),
+        $field = $('.field input', $edit_name);
+
+    $field.focus(function(){
+      $edit_name.addClass('edit');
+    });
+
+    $field.blur(function(){
+      $edit_name.removeClass('edit');
+    });
+  }());
+
 });
