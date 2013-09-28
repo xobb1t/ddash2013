6f79f98e976fa3046161b25ae070f3aa955d550d
Timofey Kukushkin <reebokey@me.com>
2013-09-28 15:12:29 +0400

    Add django auth urls

--- apps/private/urls.py
+++ apps/private/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import include, patterns, url
 urlpatterns = patterns(
     '',
     url(r'^openid/', include('openid_provider.urls')),
-    url(r'^login/$', 'private.views.login_view')
+    url(r'^login/$', 'private.views.login_view', name='private_login'),
+    url(r'^', include('django.contrib.auth.urls')),
 )
