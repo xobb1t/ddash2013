1d522c274f586aae14cf7214e26bb407bf46a7cc
Timofey Kukushkin <reebokey@me.com>
2013-09-28 12:15:39 +0400

    Use SimpleLazyObject in OrganizationMiddleware

--- apps/organizations/middleware.py
+++ apps/organizations/middleware.py
@@ -1,12 +1,14 @@
+from django.utils.functional import SimpleLazyObject
+
 from .models import Organization
 
 
 class OrganizationMiddleware(object):
 
     def process_request(self, request):
-        try:
-            self.organization = Organization.objects.get(
-                slug=request.subdomain
-            )
-        except Organization.DoesNotExist:
-            pass
+        def get_organization():
+            try:
+                return Organization.objects.get(slug=request.subdomain)
+            except Organization.DoesNotExist:
+                return None
+        request.organization = SimpleLazyObject(get_organization)
