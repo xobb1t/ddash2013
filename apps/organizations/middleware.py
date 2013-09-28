d3cb08d45af60aaf06757ad230a2a33bc3615543
Timofey Kukushkin <reebokey@me.com>
2013-09-28 16:02:23 +0400

    Remove subdomain check on pages where subdomain is none

--- apps/organizations/middleware.py
+++ apps/organizations/middleware.py
@@ -6,6 +6,8 @@ from .models import Organization
 class OrganizationMiddleware(object):
 
     def process_request(self, request):
+        if request.subdomain is None:
+            return
         try:
             request.organization = Organization.objects.get(
                 slug__iexact=request.subdomain
