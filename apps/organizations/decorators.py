a3068560cd2377634fdbc6b2ce3c2562ef312a58
Timofey Kukushkin <reebokey@me.com>
2013-09-29 18:50:57 +0400

    Fix owner_required decorator

--- apps/organizations/decorators.py
+++ apps/organizations/decorators.py
@@ -7,7 +7,8 @@ def owner_required(view_func):
     def _wrapped_view(request, *args, **kwargs):
         user = request.user
         organization = request.organization
-        if user.is_owner and organization == user.organization:
+        if user.is_authenticated() and user.is_owner and \
+                organization == user.organization:
             return view_func(request, *args, **kwargs)
         raise Http404
     return _wrapped_view
